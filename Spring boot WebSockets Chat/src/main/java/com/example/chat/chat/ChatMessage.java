7897ba6dd74970ac3e970805fc01a51e1ba0335b
maveltatkeu <maveltatkeu@gmail.com>
2025-09-17 16:52:48 +0400

    Intial commit

---
+++ Spring boot WebSockets Chat/src/main/java/com/example/chat/chat/ChatMessage.java
@@ -0,0 +1,17 @@
+package com.example.chat.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private String content;
+
+    private String sender;
+
+    private MessageType type;
+}
