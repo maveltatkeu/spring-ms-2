2791a8ac266c1ca47f0ae355f0cea0d758445ad6
maveltatkeu <maveltatkeu@gmail.com>
2025-09-18 18:12:17 +0400

    Intial CQRS complement

---
+++ CQRS/spring-command/src/main/java/org/cqrs/springcqrs/SpringCqrsApplication.java
@@ -0,0 +1,13 @@
+package org.cqrs.springcqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCqrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCqrsApplication.class, args);
+	}
+
+}
---
+++ CQRS/spring-command/src/test/java/org/cqrs/springcqrs/SpringCqrsApplicationTests.java
@@ -0,0 +1,11 @@
+package org.cqrs.springcqrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class SpringCqrsApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
