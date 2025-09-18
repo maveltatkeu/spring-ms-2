d70bcf6b820bf606e2cd22b7437402f2ec128f95
maveltatkeu <maveltatkeu@gmail.com>
2025-09-18 14:46:28 +0400

    Intial commits complement

---
+++ multi-module-microservice/commons/src/test/java/org/example/commons/CommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
